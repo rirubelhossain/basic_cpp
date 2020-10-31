0055665aa015947d10c53db6727ce3261b8fc8f5
Rubel hossain <42895415+rirubelhossain@users.noreply.github.com>
2020-10-31 22:44:16 +0600

    Add files via upload

---
+++ size_calculation_array.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std ;
+
+
+
+int main()
+{
+    int arr[] = {1 , 2 , 3 ,4 };
+    int size_arr = sizeof(arr) / sizeof(arr[0]);
+    cout<<size_arr<<endl;
+
+
+    return(0);
+}
