c54ea56bf1895d3ffff52dfa1c00e1dfdbd65d78
Rubel hossain <42895415+rirubelhossain@users.noreply.github.com>
2020-11-20 18:03:44 +0600

    Add files via upload

---
+++ problem1.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+{
+    int day ;
+
+    scanf("%d", &day);
+
+    switch(day)
+    {
+    case 1:
+        printf("sunday\n");
+        break ;
+    case 2:
+        printf("Monday\n");
+        break ;
+    case 3:
+        printf("tuesday\n");
+        break ;
+    case 4:
+        printf("wednesday\n");
+        break ;
+    case 5:
+        printf("Thursday\n");
+        break ;
+    case 6:
+        printf("Friday\n");
+        break ;
+    case 7:
+        printf("saturday\n");
+        break ;
+
+
+    }
+
+
+    return(0);
+}
