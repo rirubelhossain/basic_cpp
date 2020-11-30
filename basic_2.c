f613e1ae97afa0b70a6da4e078f0acafe0e9ea01
Rubel hossain <42895415+rirubelhossain@users.noreply.github.com>
2020-11-30 23:07:26 +0600

    Add files via upload

---
+++ basic_2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main()
+{
+    /// while loop
+    /// syntax of while loop
+
+
+int n;
+
+scanf("%d",&n);
+for(int i=1; i<=n; i++)
+{
+    printf("%d * %d = %d\n",2,i,2*i);
+}
+return 0;
+}
